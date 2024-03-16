09b4894383170794ba0819a98479b79b0d87680c
Colleen Chan <colleenechan@gmail.com>
2024-03-16 15:59:45 -0400

    fix dark mode icon loading

--- script.js
+++ script.js
@@ -4,39 +4,44 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
     if (currentTheme === 'dark') {
         document.body.classList.add('dark-mode');
-        modeSwitch.checked = true; // Ensures slider reflects dark mode is on
+        modeSwitch.checked = true;
     } else {
-        modeSwitch.checked = false; // Reflects dark mode is off
+        document.body.classList.remove('dark-mode');
+        modeSwitch.checked = false;
     }
-    
+
+    // Update icons according to the initial theme
+    updateIcons(currentTheme === 'dark' ? 'dark' : 'light');
+
     modeSwitch.addEventListener('click', toggleMode);
 });
 
 function toggleMode() {
-  document.body.classList.toggle('dark-mode');
+    document.body.classList.toggle('dark-mode');
+    const theme = document.body.classList.contains('dark-mode') ? 'dark' : 'light';
+    localStorage.setItem('theme', theme);
+    document.getElementById('modeSwitch').checked = (theme === 'dark');
 
-  const theme = document.body.classList.contains('dark-mode') ? 'dark' : 'light';
-  localStorage.setItem('theme', theme);
+    // Update icons whenever the theme is toggled
+    updateIcons(theme);
+}
 
-  // Update the slider based on the dark mode status
-  document.getElementById('modeSwitch').checked = (theme === 'dark');
+function updateIcons(theme) {
+    var icons = document.querySelectorAll(".icon");
 
-  var icons = document.querySelectorAll(".icon");
+    icons.forEach(function (icon) {
+        var src = icon.getAttribute("src");
+        var newSrc = theme === 'dark' ? src.replace(".png", "-dm.png") : src.replace("-dm.png", ".png");
 
-  icons.forEach(function (icon) {
-    var src = icon.getAttribute("src");
-    var newSrc = theme === 'dark' ? src.replace(".png", "-dm.png") : src.replace("-dm.png", ".png");
-
-    // Check if the new image source exists before setting it
-    var img = new Image();
-    img.onload = function() {
-      icon.setAttribute("src", newSrc);
-    };
-    img.onerror = function() {
-      console.error("Failed to load image: " + newSrc);
-    };
-    img.src = newSrc;
-  });
+        var img = new Image();
+        img.onload = function() {
+            icon.setAttribute("src", newSrc);
+        };
+        img.onerror = function() {
+            console.error("Failed to load image: " + newSrc);
+        };
+        img.src = newSrc;
+    });
 }
 
 $(function() {
@@ -63,4 +68,3 @@ $(function() {
     ga("send", "pageview", url);
   });
 });
-
